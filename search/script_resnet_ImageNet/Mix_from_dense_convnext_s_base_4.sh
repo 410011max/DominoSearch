ab6e0c40a135f39cccbfd128883885f8e6cc5c1f
max410011 <410011max@gmail.com>
2024-03-15 21:12:01 +0000

    update scripts

--- search/script_resnet_ImageNet/Mix_from_dense_convnext_s_base_4.sh
+++ search/script_resnet_ImageNet/Mix_from_dense_convnext_s_base_4.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=0 \
 python -m torch.distributed.launch --nproc_per_node=1 \
 ../find_mix_from_dense_imagenet.py \
---target_sparsity 0.55 \
+--target_sparsity 0.7 \
 --config ./configs/config_convnext_small_img_mix_from_dense.yaml
 
 CUDA_VISIBLE_DEVICES=1 \
@@ -13,4 +13,19 @@ python -m torch.distributed.launch --nproc_per_node=1 --master_port 25087 \
 --config ./configs/config_convnext_small_img_mix_from_dense.yaml \
 --port 25087
 
+
+
+CUDA_VISIBLE_DEVICES=0 \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port 25011 \
+../find_mix_from_dense_imagenet.py \
+--target_sparse_flops_ratio 0.5 \
+--config ./configs/config_convnext_small_img_mix_from_dense.yaml \
+--port 25011
+
+CUDA_VISIBLE_DEVICES=1 \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port 25087 \
+../find_mix_from_dense_imagenet.py \
+--target_sparse_flops_ratio 0.5 \
+--config ./configs/config_convnext_small_img_mix_from_dense.yaml \
+--port 25087
 
